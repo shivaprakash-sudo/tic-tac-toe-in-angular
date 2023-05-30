f9a9981b458a8200a35f87b012277ae744407169
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2023-05-30 15:03:59 +0100

    add pwa package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
 import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [
@@ -20,7 +22,13 @@ import { BoardComponent } from './board/board.component';
         BrowserAnimationsModule,
         NbThemeModule.forRoot({ name: 'cosmic' }),
         NbLayoutModule,
-        NbButtonModule
+        NbButtonModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: environment.production,
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
